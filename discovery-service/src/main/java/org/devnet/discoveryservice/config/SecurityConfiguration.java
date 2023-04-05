fd7ea60f1c3f7267efac16b928a02cb282f34902
Vlas <vlas_nagibin@senla.eu>
2023-04-05 16:42:08 +0300

    Implement security via kecloack

---
+++ discovery-service/src/main/java/org/devnet/discoveryservice/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package org.devnet.discoveryservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.function.Function;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .httpBasic();
+        return http.build();
+
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User
+                .builder()
+                .username("eureka")
+                .password("{noop}password")
+                .authorities("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
